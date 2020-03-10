a7a5535475821ee8ed41ffb3e9010d6b928c3145
Marcel Freiberg <marcel.freiberg@outlook.com>
2020-03-10 13:37:59 +0100

    Move bash specific files to the bash folder

---
+++ customize.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+bashrc=$(<bash/.bashrc)
+
+function installCustomBash {
+    echo -e "\n" >> ~/.bashrc && echo "$bashrc" >> ~/.bashrc
+
+    cp ./bash/.bash_custom ~/.bash_custom
+}
+
+function installTheFuck {
+    sudo apt update && sudo apt install python3-dev python3-pip python3-setuptools && sudo pip3 install thefuck
+}
+
+installCustomBash
+
+read -p "You want to install thefuck? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    installTheFuck
+fi
+
+echo 'Done!'
